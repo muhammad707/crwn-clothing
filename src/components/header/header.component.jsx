0000158001780c8d66822a9f712cceeaefec1233
Mukhammadjon <mukhammadjon.707s@gmail.com>
2020-07-21 23:57:33 +0500

    Storing all data to firestore, HOC, styled-components,

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,34 +8,32 @@ import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
-import "./header.styles.scss";
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionDiv,
+  OptionLink,
+} from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className={"logo-container"} to='/'>
+  <HeaderContainer>
+    <LogoContainer to='/'>
       <Logo className={"logo"} />
-    </Link>
-    <div className='options'>
-      <Link className='option' to={"/shop"}>
-        SHOP
-      </Link>
-      <Link className='option' to={"/contact"}>
-        CONTACT
-      </Link>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to={"/shop"}>SHOP</OptionLink>
+      <OptionLink to={"/contact"}>CONTACT</OptionLink>
 
       {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
+        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
       ) : (
-        <Link className={"option"} to={"/signin"}>
-          SIGNIN
-        </Link>
+        <OptionLink to={"/signin"}>SIGNIN</OptionLink>
       )}
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
